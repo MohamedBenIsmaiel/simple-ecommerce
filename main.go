7a04be1770e49d03e31ce2f7a5368bdce21b77fa
mohamed  ismaiel <generalbuismaiel@gmail.com>
2022-02-28 01:08:13 +0200

    main is the enetry point for our app

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 import(
 	"os"
+	"log"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/MohamedBenIsmaiel/simple-ecommerce/routes/user"
+
 )
 
 func main(){
+	err  := godotenv.Load(".env")
+	if err != nil{
+		log.Fatal(err)
+	}
+
 	port := os.Getenv("PORT")
 	if port == ""{
 		port = "5000"
@@ -16,5 +25,6 @@ func main(){
 	router.GET("/ping", func(c *gin.Context){
 		c.JSON(200, gin.H{"success": "PONG !..."})
 	})
+	userRoutes.MapUri(router)
 	router.Run(":" + port)
 }
